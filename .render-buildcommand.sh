c4683df1833ca91c264855aab4403cdd7c4bc512
Jared Fischer <jared.the.automator@gmail.com>
2025-01-28 12:33:32 -0500

    Update .render-buildcommand.sh

--- .render-buildcommand.sh
+++ .render-buildcommand.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-npm install -g pnpm turbo rimraf
+npm install -g pnpm turbo rimraf vite
 pnpm install --no-frozen-lockfile
-pnpm add https://github.com/jared-the-automator/n8n-nodes-job-info/archive/main.tar.gz --workspace-root
 pnpm build
