25f101692c94f7c0b4e07b43c659c0514c21aa6b
Jared Fischer <jared.the.automator@gmail.com>
2025-01-28 13:16:17 -0500

    Update .render-buildcommand.sh

--- .render-buildcommand.sh
+++ .render-buildcommand.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Set up SSH
+mkdir -p ~/.ssh
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 # Set environment variables
 export SHELL=/bin/bash
 export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
